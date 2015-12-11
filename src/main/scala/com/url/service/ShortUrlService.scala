4c699cac1e35710d49c72fcad2a2090dccc5aa97
Felipe Yanagiya <felipey@ciandt.com>
2015-12-11 19:17:55 -0200

    fixed toShortUrl service

--- src/main/scala/com/url/service/ShortUrlService.scala
+++ src/main/scala/com/url/service/ShortUrlService.scala
@@ -1,6 +1,6 @@
 package com.url.service
 
-import scalaj.http.Base64
+import scalaj.http.{Http, Base64}
 
 /**
  * Service to short an url address
@@ -9,12 +9,22 @@ import scalaj.http.Base64
  */
 class ShortUrlService {
 
-    def shortUrlAddress(longUrl : String): String = {
+    def validateUrl(longUrl: String): Unit = {
         if (longUrl == null) {
-          throw new IllegalArgumentException
-        } else {
-            Base64.encodeString(longUrl)
+            throw new IllegalArgumentException
         }
+        //TODO validate input url format
+
+    }
+
+    def shortUrlAddress(longUrl: String): String = {
+        validateUrl(longUrl)
+
+        val urls = longUrl.split('.')
+        val firstHalf = urls(1).substring(0)
+        val secondHalf = urls(2).substring(0, 1)
+
+        urls(0) + firstHalf + "." + secondHalf + "/" + Base64.encodeString(longUrl)
     }
 
 }
